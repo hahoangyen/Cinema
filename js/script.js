96890983143b6789429e481567fea4d3b5143f90
Hoang Tran <tmhoang2906@gmail.com>
2023-04-02 11:11:06 -0700

    update js for contactus form

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 var input = document.getElementById("txtLookfor"); //replace by input field ID
 
 // Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(enterEvent) {
+input.addEventListener("keypress", function (enterEvent) {
   // If the user presses the "Enter" key on the keyboard
   if (enterEvent.key === "Enter") {
     // Cancel the default action, if needed
@@ -16,41 +16,55 @@ input.addEventListener("keypress", function(enterEvent) {
 
 // search function
 function search() {
-    var site = 'google.com';
-    var lookfor = document.getElementById("txtLookfor").value;
-    var query="http://www.google.com/search?q=" + encodeURIComponent(lookfor);
-    location.href = query;
+  var site = 'google.com';
+  var lookfor = document.getElementById("txtLookfor").value;
+  var query = "http://www.google.com/search?q=" + encodeURIComponent(lookfor);
+  location.href = query;
+
+  // sleep function 
+} function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
 }
 
-//form validation
+
 const submit = document.getElementById("submit");
 
-submit.addEventListener('click', validate);
+submit.addEventListener('click', (validate));
 
-function validate(e) {
+//form validation function
+async function validate(e) {
   e.preventDefault();
 
+  //declare field to validate
   const nameField = document.getElementById("name");
   const emailField = document.getElementById("email");
-  const passField= document.getElementById("password");
-  const passField2= document.getElementById("password2");
-  
+  const msgField = document.getElementById("message"); 
+  //const passField= document.getElementById("password");
+  //const passField2= document.getElementById("password2");
+
+  //validation condition
   if (!nameField.value) {
     nameField.classList.add("invalid");
-    // const nameError = document.getElementById("nameError");
-    // nameError.classList.add("visible");
-    // nameError.setAttribute('aria-hidden', false);
-    // nameError.setAttribute('aria-invalid', true);
-  }  else nameField.classList.remove("invalid");
+  } else nameField.classList.remove("invalid");
+
   if (!emailField.value) {
     emailField.classList.add("invalid");
   } else emailField.classList.remove("invalid");
-  if (!passField.value) {
-    passField.classList.add("invalid");
-  } else passField.classList.remove("invalid");
-   return valid;
-}
 
+  if (!msgField.value) {
+    msgField.classList.add("invalid");
+  } else msgField.classList.remove("invalid");
+
+
+  //send condition
+  if (nameField.value != "" && emailField.value != "" && msgField.value != "") {
+    await sleep(1500);
+    document.getElementById("form-message-success").style.visibility = "visible";
+
+  }
+}
 
 
 
