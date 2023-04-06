3fd37521e866bee0648b3f185a86aa0086ab3202
Hoang Tran <tmhoang2906@gmail.com>
2023-04-06 09:59:12 -0700

    update js for slide into main.html

--- js/script.js
+++ js/script.js
@@ -20,82 +20,62 @@ function search() {
   var query = "http://www.google.com/search?q=" + encodeURIComponent(lookfor);
   location.href = query;
 }
-  // sleep function 
- function sleep(ms) {
+// sleep function 
+function sleep(ms) {
   return new Promise(
     resolve => setTimeout(resolve, ms)
   );
 }
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("slide-dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
 
 
-//contact-form
-const submit = document.getElementById("submit");
 
-submit.addEventListener('click', (validate));
+function contactForm() {
+  // here we call it because we are sure this element exists... so everything inside function must work
 
-//form validation function
-async function validate(e) {
-  e.preventDefault();
+  //contact-form
+  const submit = document.getElementById("submit");
 
-  //declare field to validate
-  const nameField = document.getElementById("name");
-  const emailField = document.getElementById("email");
-  const msgField = document.getElementById("message");
-  const msgError = document.getElementById("msgError");
-  //const passField= document.getElementById("password");
-  //const passField2= document.getElementById("password2");
-  const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  //validation condition
-  if (!nameField.value) {
-    nameField.classList.add("invalid");
-  } else nameField.classList.remove("invalid");
+  submit.addEventListener('click', (validate));
 
-  if (!emailField.value.match(validEmailRegex)) {
-    emailField.classList.add("invalid");
-  } else emailField.classList.remove("invalid");
+  //form validation function
+  async function validate(e) {
+    e.preventDefault();
 
-  if (msgField.value.length < 10) {
-    msgField.classList.add("invalid");
-    msgError.classList.add("visible");
-    msgError.setAttribute("aria-hidden", false);
-    msgError.setAttribute("aria-invalid", true);
-  } else {
-    msgField.classList.remove("invalid");
-    msgError.classList.remove("visible");}
+    //declare field to validate
+    const nameField = document.getElementById("name");
+    const emailField = document.getElementById("email");
+    const msgField = document.getElementById("message");
+    const msgError = document.getElementById("msgError");
+    //const passField= document.getElementById("password");
+    //const passField2= document.getElementById("password2");
+    const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    //validation condition
+    if (!nameField.value) {
+      nameField.classList.add("invalid");
+    } else nameField.classList.remove("invalid");
+
+    if (!emailField.value.match(validEmailRegex)) {
+      emailField.classList.add("invalid");
+    } else emailField.classList.remove("invalid");
+
+    if (msgField.value.length < 10) {
+      msgField.classList.add("invalid");
+      msgError.classList.add("visible");
+      msgError.setAttribute("aria-hidden", false);
+      msgError.setAttribute("aria-invalid", true);
+    } else {
+      msgField.classList.remove("invalid");
+      msgError.classList.remove("visible");
+    }
 
 
-  //send condition
-  if (nameField.value != "" && emailField.value.match(validEmailRegex) && msgField.value.length >= 10) {
-    await sleep(1500);
-    document.getElementById("form-message-success").style.visibility = "visible";
+    //send condition
+    if (nameField.value != "" && emailField.value.match(validEmailRegex) && msgField.value.length >= 10) {
+      await sleep(1500);
+      document.getElementById("form-message-success").style.visibility = "visible";
+    }
   }
 }
 
