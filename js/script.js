eec49cb2a78a9b0afbd308a6b8cb32df0a645696
Hoang Tran <tmhoang2906@gmail.com>
2023-04-07 15:57:16 -0700

    update main poster and button animation

--- js/script.js
+++ js/script.js
@@ -26,58 +26,74 @@ function sleep(ms) {
     resolve => setTimeout(resolve, ms)
   );
 }
+const submit = document.getElementById("submit");
+
+submit.addEventListener('click', validate);
+
+//form validation function
+async function validate(e) {
+  e.preventDefault();
+
+  //declare field to validate
+  const nameField = document.getElementById("name");
+  const emailField = document.getElementById("email");
+  const msgField = document.getElementById("message");
+  const msgError = document.getElementById("msgError");
+  //const passField= document.getElementById("password");
+  //const passField2= document.getElementById("password2");
+  const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  //validation condition
+  if (!nameField.value) {
+    nameField.classList.add("invalid");
+  } else nameField.classList.remove("invalid");
+
+  if (!emailField.value.match(validEmailRegex)) {
+    emailField.classList.add("invalid");
+  } else emailField.classList.remove("invalid");
+
+  if (msgField.value.length < 10) {
+    msgField.classList.add("invalid");
+    msgError.classList.add("visible");
+    msgError.setAttribute("aria-hidden", false);
+    msgError.setAttribute("aria-invalid", true);
+  } else {
+    msgField.classList.remove("invalid");
+    msgError.classList.remove("visible");
+  }
 
 
-
-
-
-function contactForm() {
-  // here we call it because we are sure this element exists... so everything inside function must work
-
-  //contact-form
-  const submit = document.getElementById("submit");
-
-  submit.addEventListener('click', (validate));
-
-  //form validation function
-  async function validate(e) {
-    e.preventDefault();
-
-    //declare field to validate
-    const nameField = document.getElementById("name");
-    const emailField = document.getElementById("email");
-    const msgField = document.getElementById("message");
-    const msgError = document.getElementById("msgError");
-    //const passField= document.getElementById("password");
-    //const passField2= document.getElementById("password2");
-    const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    //validation condition
-    if (!nameField.value) {
-      nameField.classList.add("invalid");
-    } else nameField.classList.remove("invalid");
-
-    if (!emailField.value.match(validEmailRegex)) {
-      emailField.classList.add("invalid");
-    } else emailField.classList.remove("invalid");
-
-    if (msgField.value.length < 10) {
-      msgField.classList.add("invalid");
-      msgError.classList.add("visible");
-      msgError.setAttribute("aria-hidden", false);
-      msgError.setAttribute("aria-invalid", true);
-    } else {
-      msgField.classList.remove("invalid");
-      msgError.classList.remove("visible");
-    }
-
-
-    //send condition
-    if (nameField.value != "" && emailField.value.match(validEmailRegex) && msgField.value.length >= 10) {
-      await sleep(1500);
-      document.getElementById("form-message-success").style.visibility = "visible";
-    }
+  //send condition
+  if (nameField.value != "" && emailField.value.match(validEmailRegex) && msgField.value.length >= 10) {
+    await sleep(1500);
+    document.getElementById("form-message-success").style.visibility = "visible";
   }
 }
+function show(type) {
+  var tab = document.querySelector(`.main-movies-content[data-toggle="${type}"] .main-movies-pane`);
+  var otherTab = document.querySelector(`.main-movies-content[data-toggle="${type === 'showing' ? 'coming' : 'showing'}"] .main-movies-pane`);
+   tab.style.display = "flex";
+  otherTab.style.display = "none";
 
+  var activeBtn = document.querySelector('.main-movies-nav .content-active');
+  if (activeBtn) {
+    activeBtn.classList.remove('content-active');
+  }
+  document.querySelector(`#${type}`).classList.add('content-active');
+}
 
+function readMore(movie) {
+  var dots = document.querySelector(`.review-movie-pane[data-movies="${movie}"] #dots`);
+  var moreText = document.querySelector(`.review-movie-pane[data-movies="${movie}"] #more`);
+  var btnText = document.querySelector(`.review-movie-pane[data-movies="${movie}"] #readMoreBtn`);
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+  }
+}
 
